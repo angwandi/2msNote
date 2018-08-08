28b768fd549f79b46773d0a848060a0ecce62764
Innocent <31923567+angwandi@users.noreply.github.com>
2018-08-09 00:35:52 +0100

    Add toolbar
    Signed-off-by: Innocent <31923567+angwandi@users.noreply.github.com>

---
+++ app/src/main/java/com/example/demad/a2msnote/AllNoteFragment.java
@@ -0,0 +1,46 @@
+package com.example.demad.a2msnote;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllNoteFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.all_note_fragment, container, false);
+        // Set up the toolbar
+        setUpToolbar(view);
+        return view;
+    }
+
+    private void setUpToolbar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        if (activity != null) {
+            activity.setSupportActionBar(toolbar);
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.nt_toolbar_menu, menu);
+        super.onCreateOptionsMenu(menu, menuInflater);
+    }
+}
